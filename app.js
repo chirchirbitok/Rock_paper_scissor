1490c3f8d1efcb5228fa92ff536b437657bf4275
Douglas Bitok <douglasbitok@gmail.com>
2022-02-08 23:00:21 +0300

    js

---
+++ app.js
@@ -0,0 +1,38 @@
+const userScore = 0;
+const compterScore  = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+    const choices =['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+console.log(getComputerChoice());
+
+function game(userChoice){
+    const compterChoice = getComputerChoice();
+    console.log("userChoice =>" + userChoice);
+    console.log("ComputerChoice =>" + compterChoice);
+}
+
+function main(){
+    rock_div.addEventListener('click', function(){
+        game("r");
+    })
+    paper_div.addEventListener('click', function(){
+        game("p");
+    })
+    scissors_div.addEventListener('click', function(){
+        game("s");
+    })
+}
+
+
+main();
